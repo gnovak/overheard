864c01c17310e42d5617dd1b45d81d36ff71ffe6
Greg Novak <greg.novak@gmail.com>
2014-04-17 12:42:11 +0200

    Update python version info

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ args = dict(name='overheard',
                          "Programming Language :: Python :: 2.5",
                          "Programming Language :: Python :: 2.6",
                          "Programming Language :: Python :: 2.7",
-                         #"Programming Language :: Python :: 3",
-                         #"Programming Language :: Python :: 3.1",
-                         #"Programming Language :: Python :: 3.2",
-                         #"Programming Language :: Python :: 3.3"
+                         "Programming Language :: Python :: 3",
+                         "Programming Language :: Python :: 3.2",
+                         "Programming Language :: Python :: 3.3",
+                         "Programming Language :: Python :: 3.4"
                      ])
 
 # On Python 3, we need distribute (new setuptools) to do the 2to3 conversion
